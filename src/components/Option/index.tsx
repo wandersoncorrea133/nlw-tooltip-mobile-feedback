6f5bdec0f3c58935d6d733496b1567ee27a98880
wandersondacosta <wanderson_c_c@hotmail.com>
2023-01-31 18:32:58 -0300

    mobile nlw feedback

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { View } from "react-native";
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Image,
+  ImageProps,
+  Text,
+} from "react-native";
 
 import { styles } from "./styles";
 
 export function Option() {
-  return <View style={styles.container}></View>;
+  return <TouchableOpacity style={styles.container}></TouchableOpacity>;
 }
